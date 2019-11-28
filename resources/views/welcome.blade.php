91f6d9bfed6ae1e10c2ef96501ef9472d5351fa4
Inigopees <iperezes18dw@ikzubirimanteo.com>
2019-11-28 10:19:25 +0100

    Landign page terminada

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -15,7 +15,7 @@
 
   <!-- Custom styles for this template -->
   <link href="css/small-business.css" rel="stylesheet">
-
+  <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css">
 </head>
 
 <body>
