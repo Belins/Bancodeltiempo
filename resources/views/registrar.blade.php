c2ab95e6418512dd8c0d05814f94955f1dd2b4da
samuelroam <samuelroam@gmail.com>
2019-12-02 11:33:59 +0100

    Traduccion de registro

--- resources/views/registrar.blade.php
+++ resources/views/registrar.blade.php
@@ -9,39 +9,39 @@
                 <div id="registro" class="col-md-12">
                 <form id="store" method="post" action="{{route('guardarregistro')}}">
                     @csrf
-                    <legend class="text-center header">Registro</legend>
+                    <legend class="text-center header">{{ trans('messages.Registro') }}</legend><!--Registro-->
                     @if ($errors ->has('name'))
                     <a class="error">{{ $errors->first('name') }}</a><br>
                     @endif
                     <fieldset class="form-group">
-                        <input type="text" class="form-control" name="name" placeholder="Nombre">
+                        <input type="text" class="form-control" name="name" placeholder="{{ trans('messages.nombre') }}"><!--Nombre-->
                     </fieldset>
                     @if ($errors ->has('lastname'))
                     <a class="error">{{ $errors->first('lastname') }}</a><br>
                     @endif
                     <fieldset class="form-group">
-                        <input type="text" class="form-control" name="lastname" placeholder="Apellido">
+                        <input type="text" class="form-control" name="lastname" placeholder="{{ trans('messages.Apellido') }}"><!--Apellido-->
                     </fieldset>
                     @if ($errors ->has('email'))
                     <a class="error">{{ $errors->first('email') }}</a><br>
                     @endif
                     <fieldset class="form-group">
-                        <input type="email" class="form-control" name="email" placeholder="email">
+                        <input type="email" class="form-control" name="email" placeholder="{{ trans('messages.Email') }}"><!--Email-->
                     </fieldset>
                     @if ($errors ->has('password'))
                     <a class="error">{{ $errors->first('password') }}</a><br>
                     @endif
                     <fieldset class="form-group">
-                        <input type="password" class="form-control" name="password" placeholder="password">
+                        <input type="password" class="form-control" name="password" placeholder="{{ trans('messages.Contraseña') }}"><!--Contraseña-->
                     </fieldset>
                     @if ($errors ->has('phone'))
                     <a class="error">{{ $errors->first('phone') }}</a><br>
                     @endif
                     <fieldset class="form-group">
-                        <input type="text" class="form-control" name="phone" placeholder="telefono">
+                        <input type="text" class="form-control" name="phone" placeholder="{{ trans('messages.Telefono') }}"><!--Telefono-->
                     </fieldset>
                     <fieldset class="form-group text-xs-right">
-                        <button type="submit" id="btnregistro" class="btn btn-primary btn-lg">Enviar</button>
+                        <button type="submit" id="btnregistro" class="btn btn-primary btn-lg">{{ trans('messages.Enviar') }}</button><!--Enviar-->
                     </fieldset>
                 </form>
             </div>
