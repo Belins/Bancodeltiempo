92f9117482333cd675c14e4ae3f97f8af4de6912
samuelroam <samuelroam@gmail.com>
2019-12-03 18:05:27 +0100

    añadir gitignore (supuestamente)

--- resources/lang/es/messages.php
+++ resources/lang/es/messages.php
@@ -65,12 +65,7 @@ return [
         en este caso lo gastas en contratar un jardinero.',
     'Ejemplo' => 'Ejemplo',
     'Perfil' => 'Ver tu perfil',
-<<<<<<< HEAD
-    'Gestion' => 'Gestión de usuarios',
-    'Datos' => 'Datos',
-=======
     'Gestion' => 'Gestión de usuario',
     'Actividad' => 'Ver tu actividad',
     'Ofertas' => 'Ver ofertas',
->>>>>>> traduccion
 ];
