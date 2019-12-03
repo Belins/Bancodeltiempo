fe73bce29e318fc03a0aadc44c6e9a6cd62ccfa0
samuelroam <samuelroam@gmail.com>
2019-12-03 08:57:56 +0100

    Correccion pequeño error en traduccion

--- resources/views/registrar.blade.php
+++ resources/views/registrar.blade.php
@@ -14,7 +14,7 @@
                     <a class="error">{{ $errors->first('name') }}</a><br>
                     @endif
                     <fieldset class="form-group">
-                        <input type="text" class="form-control" name="name" placeholder="{{ trans('messages.nombre') }}" onkeyup="this.value=NumText(this.value)" required><!--Nombre-->
+                        <input type="text" class="form-control" name="name" placeholder="{{ trans('messages.Nombre') }}" onkeyup="this.value=NumText(this.value)" required><!--Nombre-->
                     </fieldset>
                     @if ($errors ->has('lastname'))
                     <a class="error">{{ $errors->first('lastname') }}</a><br>
