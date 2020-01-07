086346a166c9e4234920d5529a378efa00a1d91b
Belins <dbelinchonal18dw@ikzubirimanteo.com>
2020-01-07 11:36:52 +0100

    Cards a interfaz de usuario

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -1,5 +1,25 @@
-@include('/includes/refcode')
+<!DOCTYPE html>
+<html>
+<head>
+	<title>Banco Del Tiempo</title>
+	@include('/includes/refcode')
+	<link href="css/card.css" rel="stylesheet">
+</head>
+<body>
+
+</body>
+</html>
+
 @include('/includes/headerUser')
-<div class="container">
-    <h1 class="text-center mt-5">Estas logeado!</h1>
+
+<div class="card">
+  <img src="img.jpg" alt="John" style="width:100%">
+  <h1>John Doe</h1>
+  <p class="title">CEO & Founder, Example</p>
+  <p>Harvard University</p>
+  <a href="#"><i class="fa fa-dribbble"></i></a>
+  <a href="#"><i class="fa fa-twitter"></i></a>
+  <a href="#"><i class="fa fa-linkedin"></i></a>
+  <a href="#"><i class="fa fa-facebook"></i></a>
+  <p><button>Contact</button></p>
 </div>
