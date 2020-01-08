a48152fddf2d30c7c587e13415c80263cc06f201
Belins <dbelinchonal18dw@ikzubirimanteo.com>
2020-01-08 08:38:47 +0100

    Añado pequeños cambios por errores

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -6,20 +6,19 @@
 	<link href="css/card.css" rel="stylesheet">
 </head>
 <body>
+@include('/includes/headerUser')
 
+<div class="card text-center">
+  <img src="{{asset('img/programador.jpg')}}" alt="David" style="width:100%">
+  <h3>David Belinchon</h3>
+  <p class="title">Desarrollador de aplicaciones web</p>
+  <p>Harvard University</p>
+  <a href="#"><i class="fa fa-linkedin"></i></a>
+  <p><button>Contacto</button></p>
+</div>
+@include('/includes/footer')
 </body>
 </html>
 
-@include('/includes/headerUser')
 
-<div class="card">
-  <img src="img.jpg" alt="John" style="width:100%">
-  <h1>John Doe</h1>
-  <p class="title">CEO & Founder, Example</p>
-  <p>Harvard University</p>
-  <a href="#"><i class="fa fa-dribbble"></i></a>
-  <a href="#"><i class="fa fa-twitter"></i></a>
-  <a href="#"><i class="fa fa-linkedin"></i></a>
-  <a href="#"><i class="fa fa-facebook"></i></a>
-  <p><button>Contact</button></p>
-</div>
+
