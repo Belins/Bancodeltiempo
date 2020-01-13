5a8be1d95007b9d0ca68ff91e46859f68fd1872d
Inigopees <iperezes18dw@ikzubirimanteo.com>
2020-01-13 12:11:14 +0100

    funciona correctamente el envio de email a los usuarios que contacten con el formulario de la landing page

--- resources/views/email/emailForm.blade.php
+++ resources/views/email/emailForm.blade.php
@@ -4,18 +4,18 @@
         <form action="{{route('contact')}}" method="post">
             {{csrf_field()}}
             <div class="row m-4">
-                <label>Email a responder : </label><br>
+                <label><b>Email a responder : </b></label>&nbsp;<br>
                 <input type="email" id="to" name="email" value="{{$datosMensaje->email}}" readonly/>
             </div>
             <div class="row m-4">
-                <label>Asunto del email : </label><br>
+                <label><b>Asunto del email : </b></label>&nbsp;<br>
                 <input type="text" name="asunto" id="asunto" value="Atención al cliente BANCO DEL TIEMPO"/>
             </div>
             <div class="row m-4">
-                <label>Mensaje a enviar : </label>
+                <label><b>Mensaje a enviar : </b></label>&nbsp; 
                 <textarea id="mensaje" name="msg" rows="10" cols="100">Buenas {{$datosMensaje->nombre}},</textarea>
             </div>
-            <div class="row m-4">
+            <div class="row m-4 justify-content-center">
                 <input type="submit" value="enviar respuesta"/>
             </div>
         </form>
